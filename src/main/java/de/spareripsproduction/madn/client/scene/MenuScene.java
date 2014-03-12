37c519717ac2ca114b156d2d6f1d9b3aee84e120
Marian Sievers <marian.sievers@gmail.com>
2014-03-12 12:22:20 +0100

    Add implementation of methods of the interface.

--- src/main/java/de/spareripsproduction/madn/client/scene/MenuScene.java
+++ src/main/java/de/spareripsproduction/madn/client/scene/MenuScene.java
@@ -1,7 +1,18 @@
 package de.spareripsproduction.madn.client.scene;
 
+import java.awt.*;
+
 /**
  * Created by marian on 12/03/14.
  */
 public class MenuScene extends Scene {
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
 }
