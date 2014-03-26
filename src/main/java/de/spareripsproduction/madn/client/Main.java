4f23d61f04b227f6495ea319360b508e418c0d3e
Angart <tissen.arthur@gmail.com>
2014-03-26 11:16:35 +0100

    j

--- src/main/java/de/spareripsproduction/madn/client/Main.java
+++ src/main/java/de/spareripsproduction/madn/client/Main.java
@@ -11,31 +11,5 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("This is client main!");
 
-
-        JFrame frame = new JFrame("Dr. Rips ärgere dich nicht!");
-        JTextField field = new JTextField();
-        field.setSize(100,23);
-        field.setText("Fertig - abputzen");
-        frame.setLayout(null);
-        frame.add(field);
-        frame.setSize(200,200);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-
-        //region LoL
-
-        for (int i = 0; i < 100; i++) {
-            try {
-                Thread.sleep(100);
-                System.out.println("Dr. Rips ärgere dich nicht!"+ i);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println(field.getText());
-        //endregion
-
-        //fertig - abputzen
     }
 }
