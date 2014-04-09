cc783a9044f492d9db857f1ab1f3ac5255c7571b
Thomas Hampe <th.hampe@gmail.com>
2014-04-09 11:33:30 +0200

    GameFirgure Render

--- src/main/java/de/spareripsproduction/madn/client/logic/Player.java
+++ src/main/java/de/spareripsproduction/madn/client/logic/Player.java
@@ -144,12 +144,19 @@ public class Player implements RenderAndUpdateable {
                 }
             }
         }
+
+        for (GameFigure g : this.gameFigureList) {
+            g.update();
+        }
     }
 
     @Override
     public void render(Graphics2D g) {
         if (this.btnDice != null) {
             this.btnDice.render(g);
+        }
+        for (GameFigure f : this.gameFigureList) {
+            f.render(g);
         }
     }
 }
